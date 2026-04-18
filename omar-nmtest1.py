3660c2be8979b7a458cbfb04455d5b83337ff23f
NM-Om4R <omar.nunciom@uanl.edu.mx>
2026-04-17 20:46:28 -0600

    se agregan los getters

--- omar-nmtest1.py
+++ omar-nmtest1.py
@@ -5,5 +5,17 @@ class Xbox:
         self.__color = color
         self.__precio = precio 
         self.__encendido = encendido
-        
+    
+    def get_modelo(self):
+        return self.__modelo
+    def get_almacenamiento(self):
+        return self.__almacenamiento
+    def get_color(self):
+        return self.__color
+    def get_precio(self):
+        return self.__precio
+    def get_encendido(self):
+        return self.__encendido
+    
+    
 
