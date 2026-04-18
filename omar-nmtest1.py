d3eceaea3315a86b96c29a43f0025bfa398ee256
NM-Om4R <omar.nunciom@uanl.edu.mx>
2026-04-17 20:37:23 -0600

    se agregaron los atributos

--- omar-nmtest1.py
+++ omar-nmtest1.py
@@ -1,2 +1,9 @@
 class Xbox:
-    
+    def __init__(self, modelo, almacenamiento, color, precio, encendido):
+        self.__modelo = modelo 
+        self.__almacenamiento = almacenamiento
+        self.__color = color
+        self.__precio = precio 
+        self.__encendido = encendido
+        
+
